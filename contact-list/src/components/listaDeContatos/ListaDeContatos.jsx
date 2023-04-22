8eb36b6b131b2141025e13a6873a65935e2f3cc0
Roger Franco <rogerfrancogalt@gmail.com>
2023-04-22 13:17:18 -0400

    CSS

--- contact-list/src/components/listaDeContatos/ListaDeContatos.jsx
+++ contact-list/src/components/listaDeContatos/ListaDeContatos.jsx
@@ -1,4 +1,7 @@
+import './ListaDeContatos.css'
 import React, { useEffect, useRef, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList, faTrash, faPlus, faTrashAlt, faUser, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export const ListaDeContatos = () => {
   const [contato, setContato] = useState({id: '', nome: '', fone: ''})
@@ -57,26 +60,57 @@ export const ListaDeContatos = () => {
 
 
   return (
-    <div>
-      <h1>Minha lista de contatos</h1>
-      <hr />
-      <div>
-        <label>Nome:</label><br />
-        <input ref={inputFocusRefNome} type="text" onChange={(e) => setContato({...contato, nome: e.target.value})} value={contato.nome} />
+    <div className='mx-2'>
+      <div className="container-fluid titulo">
+      <div className="row">
+        <div className="col text-cente">
+      <h4 className='text-center'> <FontAwesomeIcon icon={faList} className='me-2' />MINHA LISTA DE CONTATOS</h4>
+        </div>
       </div>
-      <div>
-        <label>Telefone:</label><br />
-        <input  ref={inputFocusRefFone} type="text" onChange={(e) => setContato({...contato, fone: e.target.value})} onKeyUp={enterAdicionarContato} value={contato.fone} />
+    </div>
+      <div className="container-fluid formulario">
+        <div className="row">
+          <div className="col p-3">
+            <div className="row justify-content-center">
+              <div className="col-10 col-sm-8 cl-md-6 col-lg-4">
+                <div className='mb-3'>
+                  <label className='form-label'>Nome:</label><br />
+                  <input className='form-control' ref={inputFocusRefNome} type="text" onChange={(e) => setContato({...contato, nome: e.target.value})} value={contato.nome} />
+                </div>
+                <div className='mb-3'>
+                  <label className='form-label'>Telefone:</label><br />
+                  <input className='form-control' ref={inputFocusRefFone} type="text" onChange={(e) => setContato({...contato, fone: e.target.value})} onKeyUp={enterAdicionarContato} value={contato.fone} />
+                </div>
+                <div className="row mt-3">
+                  <div className="col text-start">
+                    <button className='btn btn-danger' onClick={deletarLista}>
+                    <FontAwesomeIcon icon={faTrash} className='me-2' />
+                      Deletar Contatos
+                      </button>
+                  </div>
+                  <div className="col text-end">
+                    <button className='btn btn-primary' onClick={adicionarLista}>
+                    <FontAwesomeIcon icon={faPlus} className='me-2' />
+                    Adicionar Contato</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <button onClick={adicionarLista}>Adicionar Contato</button>
-      <button onClick={deletarLista}>Deletar Contatos</button>
-      <hr />
       {/* render lista de contatos */}
       {listaContato.map((item, index) => (
-      <div key={index}>
-        Nome: {item.nome} - Fone: {item.fone}
-        <button onClick={() => deletarContato(item.id)}>Deletar contato</button> 
-        </div>
+
+<div className="container componente-contato my-4">
+      <div className="row">
+        <div className="col p-2"><h5><FontAwesomeIcon className='me-3' icon={faUser} />Nome: {item.nome}</h5></div>
+        <div className="col p-2"><h5><FontAwesomeIcon className='me-3' icon={faPhone} />Fone: {item.fone}</h5></div>
+        <div className="col p-2 text-end">
+        <h5><FontAwesomeIcon className='me-3' icon={faTrashAlt} onClick={() => deletarContato(item.id)} /></h5>
+          </div>
+      </div>
+    </div>
         ))}
     </div>
   )
